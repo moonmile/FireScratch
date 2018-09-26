77238cd258b3d244a9603f8c827cbc6cb2e9f96e
Tomoaki Masuda <masuda@moonmile.net>
2018-09-26 23:32:51 +0900

    HttpListner のメモリーリークに対応。メインを C# にして、.NET Framework 上で動くように変更。

---
+++ src/FireScratch.Proxy/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FireScratch.Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start Firebase Scratch Proxy");
+            FireScratch.Core.FireScratch.Server(5411);
+            Console.ReadKey();
+        }
+    }
+}
